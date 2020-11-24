9c004bdc615f1672e26260b9a97ba5f96bb861e7
Chives <45418805+HOLL4ND@users.noreply.github.com>
2020-11-24 22:20:45 +0800

    02-01 and 02-02 done

---
+++ DataMining exp 02/02-01 scatter graph.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('./resources/Exp01/MergeData.csv')
+
+x = df['C1'].values
+y = df['Constitution'].values
+print(type(y))
+plt.scatter(x,y)
+plt.xlabel('C1 Score')
+plt.ylabel('Constitution')
+plt.savefig('./DataMining exp 02/02-01.png',bbox_inches='tight')
+plt.show()
